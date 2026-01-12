cf1c9eef135ee1566e14f3db8cf200b017f89b98
sshubuntu <egorshubin055@gmail.com>
2026-01-12 13:57:11 +0300

    feat: delete progLang

--- ProgrammingLanguages/Lab5/ExpressionEvaluator/PrattRules.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExpressionEvaluator;
-
-public static class PrattRules
-{
-    public const int AddSub = 1;
-    public const int MulDiv = 2;
-    public const int Unary = 3;
-}
--- ProgrammingLanguages/Lab5/ExpressionEvaluator/Token.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace ExpressionEvaluator;
-
-public record struct Token(TokenKind Kind, string Text, long Number, int Position = 0);
